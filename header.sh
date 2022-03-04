ea4e981a8a9c20aa13ad79d2edf534b42a43d8ec
Tywael <yves.althaus42@gmail.com>
2022-03-04 05:30:08 +0100

    first commit

---
+++ header.sh
@@ -0,0 +1,39 @@
+def_name(){
+    len=${#name}
+    max=$((80 - $len - 29))
+    for (( c=1; c<=max; c++ ))
+    do
+        name+=" "
+    done
+}
+
+date=$(date +%Y/%m/%d)
+echo -e "$header"
+if [ $1 == "-d" ]; then
+    param=( $( find $2 -name "*.c" ) $( find $2 -name "*.h" ) )
+else
+    param=$@
+fi
+
+for file in "${param[@]}"
+do
+    name=$( basename $file)
+    def_name
+    header="/* ************************************************************************** */\n"
+    header+="/*                                                                            */\n"
+    header+="/*                                                        :::      ::::::::   */\n"
+    header+="/*   $name:+:      :+:    :+:   */\n"
+    header+="/*                                                    +:+ +:+         +:+     */\n"
+    header+="/*   By: yalthaus <marvin@42lausanne.ch>            +#+  +:+       +#+        */\n"
+    header+="/*                                                +#+#+#+#+#+   +#+           */\n"
+    header+="/*   Created: $date 12:23:24 by yalthaus          #+#    #+#             */\n"
+    header+="/*   Updated: $date 14:35:19 by yalthaus         ###   ########.fr       */\n"
+    header+="/*                                                                            */\n"
+    header+="/******************************************************************************/\n"
+    if [[ $(head -c 80 $file) != "${header::80}" ]]; then
+        echo -e "$(echo -e "$header" | cat - $file)" > $file
+        echo "header add in $file"
+    else
+        echo "header already was in $file"
+    fi
+done
